d20aa7711ed8451ce8bf5edfe0df727a0204c270
Administrator <2895094701@qq.com>
2021-01-17 21:50:55 +0800

    增加群聊功能

--- src/cn/edu/hcnu/client/ChatThreadWindow.java
+++ src/cn/edu/hcnu/client/ChatThreadWindow.java
@@ -1,6 +1,8 @@
 package cn.edu.hcnu.client;
 
 import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.net.*;
 import java.sql.*;
@@ -46,16 +48,144 @@ public class ChatThreadWindow {
         JPanel p = new JPanel(new BorderLayout());
         p.add(pl, BorderLayout.WEST);
         p.add(tf);
+        tf.addKeyListener(
+                new KeyListener(){
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+
+                    }
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+                        if(e.getKeyCode()==KeyEvent.VK_ENTER){
+                            String messageTo=(String) cb.getSelectedItem();
+                            if("All".equals(messageTo)){
+                                sendAll();
+                            }
+                            else{
+                               /* sendst();*/
+                            }
+                        }
+
+                    }
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+
+                    }
+                }
+        );
         f.getContentPane().add(p, BorderLayout.SOUTH);
         f.getContentPane().add(sp);
         f.setVisible(true);
         GetMessageThread getMessageThread = new GetMessageThread(this);
         getMessageThread.start();
-
         /*
         提示XXX进入聊天室
          */
         showXXXIntoChatRoom();
+        /*
+        提示***正在聊天室
+        */
+        showXXXInChatRoom();
+    }
+    /*public void sendst() {
+        String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+        String username_db = "opts";
+        String password_db = "opts1234";
+        PreparedStatement pstmt = null;
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, username_db, password_db);
+            String sql = "SELECT username,ip,port FROM users WHERE status='online'";
+            pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                String username=rs.getString("USERNAME");
+                String ip = rs.getString("IP");
+                int port = rs.getInt("PORT");
+                System.out.println(ip);
+                System.out.println(port);
+                byte[] ipB = new byte[4];
+
+                String ips[] = ip.split("\\.");
+                for (int i = 0; i < ips.length; i++) {
+                    ipB[i] = (byte)Integer.parseInt(ips[i]);
+                }
+                if(!username.equals(name)){
+                    String message = tf.getText();
+                    byte[] m = message.getBytes();
+                    DatagramPacket dp = new DatagramPacket(m, m.length);
+                    dp.setAddress(InetAddress.getByAddress(ipB));
+                    dp.setPort(port);
+                    DatagramSocket ds = new DatagramSocket();
+                    ds.send(dp);//投递
+                }
+            }
+        } catch (SQLException | UnknownHostException | SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
+    public void sendAll() {
+        String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+        String username_db = "opts";
+        String password_db = "opts1234";
+        PreparedStatement pstmt = null;
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, username_db, password_db);
+            String sql = "SELECT username,ip,port FROM users WHERE status='online'";
+            pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                String username=rs.getString("USERNAME");
+                String ip = rs.getString("IP");
+                int port = rs.getInt("PORT");
+                System.out.println(ip);
+                System.out.println(port);
+                byte[] ipB = new byte[4];
+
+                String ips[] = ip.split("\\.");
+                for (int i = 0; i < ips.length; i++) {
+                    ipB[i] = (byte)Integer.parseInt(ips[i]);
+                }
+                if(!username.equals(name)){
+                    String message = tf.getText();
+                    byte[] m = message.getBytes();
+                    DatagramPacket dp = new DatagramPacket(m, m.length);
+                    dp.setAddress(InetAddress.getByAddress(ipB));
+                    dp.setPort(port);
+                    DatagramSocket ds = new DatagramSocket();
+                    ds.send(dp);//投递
+                }
+            }
+        } catch (SQLException | UnknownHostException | SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showXXXInChatRoom() {
+        String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+        String username_db = "opts";
+        String password_db = "opts1234";
+        PreparedStatement pstmt = null;
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, username_db, password_db);
+            String sql = "SELECT username,ip,port FROM users WHERE status='online' AND username!=?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,name);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                String username=rs.getString("USERNAME");
+                String message = username+"进入了聊天室";
+                ta.append(message);
+                cb.addItem(username);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void showXXXIntoChatRoom() {
